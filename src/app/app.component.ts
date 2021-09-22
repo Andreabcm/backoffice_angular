9ebf381cf162230abc0207116924729511f1894a
unknown <andreabcm16@gmail.com>
2021-09-22 16:59:33 +0200

    Consuming API in Google Maps JavaScript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Loader } from '@googlemaps/js-api-loader';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'backoffice';
+
+  ngOnInit(): void{
+    let loader = new Loader({
+      apiKey: 'AIzaSyDygsikUrcCdqSQ41E4MJFKBylBwXD9Vok'
+    })
+
+    loader.load().then(() => {
+      new google.maps.Map(document.getElementById("map")!,{
+        center: { lat: 51.233334, lng: 6.78333 },
+        zoom: 3
+      })
+    })
+  }
 }
