117108d66573411d39ec6b382ecb3660910fd899
unknown <andreabcm16@gmail.com>
2021-09-23 08:09:15 +0200

    creating a fake api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { Loader } from '@googlemaps/js-api-loader';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
-  title = 'backoffice';
+  title = 'gmaps';
+
 
   ngOnInit(): void{
     let loader = new Loader({
@@ -16,9 +18,11 @@ export class AppComponent implements OnInit{
 
     loader.load().then(() => {
       new google.maps.Map(document.getElementById("map")!,{
-        center: { lat: 51.233334, lng: 6.78333 },
-        zoom: 3
+        center: { lat: 40.463667, lng: -3.74922 },
+        zoom: 6
       })
     })
+
   }
+
 }
