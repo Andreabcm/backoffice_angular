0ac2ca9f60fe5ec7c67fe580aed0af36f6a6f80a
unknown <andreabcm16@gmail.com>
2021-09-23 10:31:54 +0200

    creating a new backoffice instance, so that the form displays an example character

--- src/app/backoffice-form/backoffice-form.components.ts
+++ src/app/backoffice-form/backoffice-form.components.ts
@@ -12,6 +12,17 @@ export class BackofficeFormComponent {
     genders = [ 'Male', 'Female' ];
     model = new Backoffice(18, 'Dr IQ', this.genders[0], 'Chuck Overstreet');
     submitted = false;
+
     onSubmit() { this.submitted = true; }
+
+    newBackoffice() { this.model = new Backoffice(42, '', '', '');}
+
+    jonSnow(): Backoffice {
+        const myHero =  new Backoffice(42, 'Jon Snow',
+                                    'jon.snow@westeros.com',
+                                    'Male');
+        console.log('My hero is called ' + Backoffice.name); // "My character is called Jon Snow"
+        return myHero;
+    }
 
 }
