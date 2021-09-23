df54c877390f98223976f2acba6bf46c6545a9dc
unknown <andreabcm16@gmail.com>
2021-09-23 10:45:35 +0200

    enabling the Forms feature and registering the created form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
+import { BackofficeFormComponent } from './backoffice-form/backoffice-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    BackofficeFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
