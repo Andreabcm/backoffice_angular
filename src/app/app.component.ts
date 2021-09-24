576265b57934b0097ae5f15c92e0555151b0e6bc
unknown <andreabcm16@gmail.com>
2021-09-24 11:04:52 +0200

    making a marker on the map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,16 +17,19 @@ export class AppComponent implements OnInit{
       apiKey: 'AIzaSyA7LDWSlBJ_nnEGSh7oN99IHKPyR5k4nWc'
     })
 
-    const position = { lat: 50.395639, lng: 1.1922392}
+  const position = { lat: 50.395639, lng: 1.1922392}
 
     loader.load().then(() => {
       const map = new google.maps.Map(document.getElementById("map")!,{
         center: { lat: 50.395639, lng: 1.1922392 },
         zoom: 6
-      })
-      new google.maps.Marker({ position: position, map, title:'myposition'
-      })
     })
+    
+    new google.maps.Marker({ position: position, map, title:'myposition'
+    })
+
+    })
+    
 
   }
 
